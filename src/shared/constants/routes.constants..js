dc31e7d6a3d32da3c2ad3f17990a3c21ad5af0ec
Danyil <dkichalo@gmail.com>
2020-05-08 11:44:14 +0300

    Add router constant which contain all pages routes

---
+++ src/shared/constants/routes.constants..js
@@ -0,0 +1,13 @@
+export const router = {
+    landing: '/',
+    careCheck: '/care-check',
+    losePet: '/lose-pet',
+    petFinder: '/pet-finder',
+    petProfile: '/pet-profile/:id',
+    petRegistration: '/pet-registration',
+    shelter: '/shelter',
+    userProfile: '/user-profile',
+    userRegistration: '/user-registration',
+    vanishedPetProfile: '/vanished-pet-profile/:id',
+    vanishedPets: '/vanished-pets',
+};
