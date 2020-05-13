f9a28e5f4c3322509e30f6fd0fde395fbbc7eb24
Danyil <dkichalo@gmail.com>
2020-05-13 16:45:04 +0300

    Add landing page 'about us'

--- src/pages/landing/landing.component.js
+++ src/pages/landing/landing.component.js
@@ -1,7 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 
-import { Header } from '../../shared/components/header/header.component';
- 
+import { Header } from "../../shared/components/header/header.component";
+
+import { LandingMenu } from './components/landing-menu/landing-menu.component';
+import { AboutUs } from './components/about-us/about-us.component';
+
+import "./landing.styles.css";
+
 export const Landing = () => {
-  return <Header />;
+  const [landingNumber, setLandingNumber] = useState(0);
+
+  const onLandingMenuClick = (event) => {
+    if (event.target.tagName === 'LI') {
+      const menuLinksArray = document.querySelectorAll('.landing-menu__list__item');
+      
+      for (let i = 0; i < menuLinksArray.length; i++) {
+        if (event.target === menuLinksArray[i]) {
+          setLandingNumber(i);
+        }
+      }
+    }
+  };
+
+  const renderHelper = () => {
+    switch (landingNumber) {
+      case 0: 
+        return <AboutUs />;
+      default: 
+        return 'something went wrong..'
+    }
+  }
+
+  return (
+    <div className="landing">
+      <Header />
+      <div className="banner-container">
+        <div className="banner-text-container">
+          <h1 className="banner-text">
+            Комфортніше життя вашого улюбленця разом з 
+            <span className="banner-company-name"> Petsy</span>
+          </h1>
+        </div>
+      </div>
+      <LandingMenu onLandingMenuClick={onLandingMenuClick} />
+      {renderHelper()}
+    </div>
+  );
 };
