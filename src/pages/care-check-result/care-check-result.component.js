82af619c8d52c3c80105c7d7766ea1254bcd1507
Danyil <dkichalo@gmail.com>
2020-05-22 12:08:43 +0300

    Create dog0info container of the care-check-result page

--- src/pages/care-check-result/care-check-result.component.js
+++ src/pages/care-check-result/care-check-result.component.js
@@ -1,19 +1,51 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
+
+import { Header } from "../../shared/components/header/header.component";
+import { Footer } from "../../shared/components/footer/footer.component";
+import API from "../../shared/apis/server-api";
+import { DogInfo } from "./components/dog-info/dog-info.component";
 
 import "./care-check-result.styles.css";
 
 export const CareCheckResult = () => {
+  const [breed, setBreed] = useState({});
+
   const careCheckValues = useSelector((state) => state.careCheckReducer);
 
-  const logV = () => {
-    console.log(careCheckValues);
+  useEffect(() => {
+    const getBreed = async () => {
+      const currentBreed = await API.get(
+        `/dogsInfo?breed=${careCheckValues.breed}`
+      );
+
+      setBreed(currentBreed.data[0]);
+    };
+
+    getBreed();
+  }, []);
+
+  //   const cLog = () => {
+  //     console.log(breed);
+  //   };
+
+  const renderContent = () => {
+    if (!breed || !Object.values(breed).length) {
+      return <h2 className="ccr-loading">Дані загружаються</h2>;
+    }
+
+    return (
+      <div className="ccr-wrapper">
+        <DogInfo about={breed.about} breed={breed.breed} />
+      </div>
+    );
   };
 
   return (
-    <div>
-      {logV()}
-      Care check result component
+    <div className="care-check-result-page">
+      <Header />
+      {renderContent()}
+      <Footer />
     </div>
   );
 };
