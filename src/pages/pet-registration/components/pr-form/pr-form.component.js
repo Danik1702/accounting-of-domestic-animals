60b641bb0c9732118f054cc0ef3d36e74b628a9c
Danyil <dkichalo@gmail.com>
2020-05-31 12:52:32 +0300

    Add styles to the pet registartion page

--- src/pages/pet-registration/components/pr-form/pr-form.component.js
+++ src/pages/pet-registration/components/pr-form/pr-form.component.js
@@ -1,11 +1,16 @@
 import React, { useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import { useForm } from "react-hook-form";
 
+import { ROUTES } from "../../../../shared/constants/routes.constants";
 import API from "../../../../shared/apis/server-api";
 
 import "./pr-form.styles.css";
 
 export const PetRegistrationForm = () => {
   const [breeds, setBreeds] = useState([]);
+  const { register, handleSubmit, errors } = useForm();
+  const history = useHistory();
 
   useEffect(() => {
     const getBreeds = async () => {
@@ -31,37 +36,82 @@ export const PetRegistrationForm = () => {
     });
   };
 
+  const onUndoClick = () => {
+    history.push(ROUTES.userProfile);
+  };
+
+  const onFormSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
     <div className="pr-form-container">
-      <form className="pr-form">
+      <form className="pr-form" onSubmit={handleSubmit(onFormSubmit)}>
         <div className="pr-form-header-container">
           <h3 className="pr-form-header">
             Зареєструйте свого домашнього улюбленця
           </h3>
         </div>
         <label className="pr-form-label">Порода</label>
-        <select name="breed" className="pr-form-field">
+        <select
+          name="breed"
+          className="pr-form-field pr-form-select"
+          ref={register({ required: true })}
+        >
           <option disabled value="Виберіть породу">
             Виберіть породу
           </option>
           {renderSelect()}
         </select>
+        {errors.breed && (
+          <p className="pr-form-validation">Виберіть породу вашої собачки</p>
+        )}
 
         <label className="pr-form-label">Кличка</label>
-        <input name="name" className="pr-form-fiedl" />
+        <input
+          name="name"
+          className="pr-form-field"
+          ref={register({ required: true })}
+        />
+        {errors.name && (
+          <p className="pr-form-validation">Вкажіть кличку собачки</p>
+        )}
 
         <label className="pr-form-label">Дата народження</label>
-        <input type="date" className="pr-form-date" name="birthday" />
+        <input
+          type="date"
+          className="pr-form-date"
+          name="birthday"
+          ref={register({ required: true })}
+        />
+        {errors.date && (
+          <p className="pr-form-validation">Вкажіть дату народження песика</p>
+        )}
 
         <label className="pr-form-label">Стать</label>
         <div className="pr-form-radio-container">
-          <input type="radio" name="gender" value="Хлопчик" />
+          <input
+            type="radio"
+            name="gender"
+            value="Хлопчик"
+            className="pr-form-radio"
+            ref={register({ required: true })}
+          />
           <label className="pr-form-radio-text">Хлопчик</label>
         </div>
         <div className="pr-form-radio-container">
-          <input type="radio" name="gender" value="Хлопчик" />
-          <label className="pr-form-radio-text">Хлопчик</label>
+          <input
+            type="radio"
+            name="gender"
+            value="Дівчинка"
+            className="pr-form-radio"
+            ref={register({ required: true })}
+          />
+          <label className="pr-form-radio-text">Дівчинка</label>
         </div>
+        {errors.gender && (
+          <p className="pr-form-validation">Вкажіть стать вашої собачки</p>
+        )}
 
         <div className="pr-form-submit-container">
           <input
@@ -69,6 +119,9 @@ export const PetRegistrationForm = () => {
             className="pr-form-submit"
             value="Зареєструвати тваринку"
           />
+          <div className="pr-form-undo" onClick={onUndoClick}>
+            Назад
+          </div>
         </div>
       </form>
     </div>
