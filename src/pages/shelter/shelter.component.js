c6acd390954eed976c52bed5e746dc591d0c46b7
Danyil <dkichalo@gmail.com>
2020-05-31 17:37:46 +0300

    Connect shelter page with server data

--- src/pages/shelter/shelter.component.js
+++ src/pages/shelter/shelter.component.js
@@ -1,21 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import { Header } from "../../shared/components/header/header.component";
 import { Footer } from "../../shared/components/footer/footer.component";
 import { ShelterHeader } from "./components/shelter-header/shelter-header.component";
-import { SeparateShelter } from './components/separate-shelter/separate-shelter.component';
+import { SeparateShelter } from "./components/separate-shelter/separate-shelter.component";
+import API from "../../shared/apis/server-api";
 
 import "./shelter.styles.css";
 
 export const Shelter = () => {
-  return (
-    <>
-      <Header />
-      <div className="shelter-page-wrapper">
-        <ShelterHeader />
-        <SeparateShelter />
-      </div>
-      <Footer />
-    </>
-  );
+  const [shelters, setShelters] = useState({});
+
+  useEffect(() => {
+    const getShelters = async () => {
+      const shelters = await API.get("/shelters");
+
+      setShelters(shelters.data);
+    };
+
+    getShelters();
+  }, []);
+
+  const renderContent = () => {
+    if (!Object.keys(shelters).length) {
+      return <div>Загрузка даних</div>;
+    }
+
+    return (
+      <>
+        <Header />
+        <div className="shelter-page-wrapper">
+          <ShelterHeader />
+          <SeparateShelter 
+            name='ЛКП Лев'
+            dogs={shelters.lkpLion}
+          />
+        </div>
+        <Footer />
+      </>
+    );
+  };
+
+  return renderContent();
 };
