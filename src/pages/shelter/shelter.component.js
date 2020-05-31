ed199f0ac8ead34b19c6db57954eccad1ed9b06d
Danyil <dkichalo@gmail.com>
2020-05-31 17:28:34 +0300

    Create separate shelter piece of the shelter page

--- src/pages/shelter/shelter.component.js
+++ src/pages/shelter/shelter.component.js
@@ -3,6 +3,7 @@ import React from "react";
 import { Header } from "../../shared/components/header/header.component";
 import { Footer } from "../../shared/components/footer/footer.component";
 import { ShelterHeader } from "./components/shelter-header/shelter-header.component";
+import { SeparateShelter } from './components/separate-shelter/separate-shelter.component';
 
 import "./shelter.styles.css";
 
@@ -12,6 +13,7 @@ export const Shelter = () => {
       <Header />
       <div className="shelter-page-wrapper">
         <ShelterHeader />
+        <SeparateShelter />
       </div>
       <Footer />
     </>
