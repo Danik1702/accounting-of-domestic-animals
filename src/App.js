0a4aa9d29d85aeb341a4f0a91f855a96c2d748fe
Danyil <dkichalo@gmail.com>
2020-06-04 18:11:34 +0300

    Delete unnecessary routes on the app.js file

--- src/App.js
+++ src/App.js
@@ -3,14 +3,12 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import { CareCheck } from './pages/care-check/care-check.component';
 import { Landing } from './pages/landing/landing.component';
-import { LosePet } from './pages/lose-pet/lose-pet.component';
 import { PetFinder } from './pages/pet-finder/pet-finder.component';
 import { PetProfile } from './pages/pet-profile/pet-profile.component';
 import { PetRegistration } from './pages/pet-registration/pet-registration.component';
 import { Shelter } from './pages/shelter/shelter.component';
 import { UserProfile } from './pages/user-profile/user-profile.component';
 import { UserRegistration } from './pages/user-registration/user-registration.component';
-import { VanishedPetProfile } from './pages/vanished-pet-profile/vanished-pet-profile.component';
 import { VanishedPets } from './pages/vanished-pets/vanished-pets.component';
 import { ReportMissing } from './pages/report-missing/report-missing.component';
 import { CareCheckResult } from './pages/care-check-result/care-check-result.component';
@@ -26,14 +24,12 @@ const App = () => {
         <Route path={ROUTES.landing} exact component={Landing} />
         <Route path={ROUTES.careCheck} exact component={CareCheck} />
         <Route path={ROUTES.careCheckResult} exact component={CareCheckResult} />
-        <Route path={ROUTES.losePet} exact component={LosePet} />
         <Route path={ROUTES.petFinder} exact component={PetFinder} />
         <Route path={ROUTES.petProfile} exact component={PetProfile} />
         <Route path={ROUTES.petRegistration} exact component={PetRegistration} />
         <Route path={ROUTES.shelter} exact component={Shelter} />
         <Route path={ROUTES.userProfile} exact component={UserProfile} />
         <Route path={ROUTES.userRegistration} exact component={UserRegistration} />
-        <Route path={ROUTES.vanishedPetProfile} exact component={VanishedPetProfile} />
         <Route path={ROUTES.vanishedPets} exact component={VanishedPets} />
         <Route path={ROUTES.reportMissing} exact component={ReportMissing} />
       </Switch>
